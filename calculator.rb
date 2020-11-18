52ef888cc44302bb053cc6ea4095ad1488b19d38
John Wisneski <john.j.wisneski@gmail.com>
2020-11-18 02:26:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,13 +2,13 @@ first_number = 7
 second_number = 10 
 
 def sum 
-  first_number + second_number
+  first_number+second_number
 end
 
 def difference 
-  first_number - second_number
+  first_number-second_number
 end
 
 def product 
-  first_number * second_number
+  first_number*second_number
 end
