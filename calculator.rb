90d2a801838dfbae232f54aa540855f10e0dce83
John Wisneski <john.j.wisneski@gmail.com>
2020-11-18 02:23:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+first_number = 7
+second_number = 10 
+
+def sum 
+  first_number + second_number
+end
+
+def difference 
+  first_number - second_number
+end
+
+def product 
+  first_number * second_number
+end
